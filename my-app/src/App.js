086559bb9e4f0c1743ddcf65621583de8857a64d
niezgodam <michalniezgoda100@gmail.com>
2024-03-18 20:37:24 +0100

    main functionality html, css, app.js

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar.js";
+import Back from "./components/Back.js";
+
+function App() {
+  return (
+    <div>
+      <Back />
+    </div>
+  );
+}
+
+export default App;
